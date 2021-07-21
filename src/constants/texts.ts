2f2c15a902a1d20241dc6b02d08a7be28bc5b5cf
judithlient <judith@rzilient.club>
2021-07-21 15:10:58 +0200

    Add constants for text on profile tab

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -1,5 +1,22 @@
 export default {
     searching: {
         title: 'Sobre el puesto que busca'
+    },
+    perfil: {
+        tabs: {
+            0: 'Perfil Nuwe',
+            1: 'Timeline CV',
+            2: 'Social'
+        },
+        hard: 'Hard skills validadas en NUWE',
+        top: 'Top 5 hard skills de',
+        otras: 'Otras hard skills validadas',
+        soft: 'Soft skills validadas en NUWE',
+        puntuaciones: 'Puntuaciones obtenidas de los retos grupales',
+        analitica: 'Capacidad analítica',
+        teamwork: 'Teamwork',
+        comunicacion: 'Comunicación',
+        problemas:'Resolución de problemas', 
+        other: 'Lo que sea'
     }
 }
