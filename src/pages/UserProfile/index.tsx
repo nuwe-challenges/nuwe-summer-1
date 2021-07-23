79ba83318897063fa49f4422158fc65a4104ef8c
judithlient <judith@rzilient.club>
2021-07-23 16:34:38 +0200

    Move to own component

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { getUser } from '../../redux/reducers/user.reducer';
-import { GithubWhite, LinkedinWhite } from '../../assets'
 import UserStack from '../../components/UserStack'
-import './index.scss'
+import UserInfo from '../../components/UserInfo'
 
 const UserProfile = (): JSX.Element => {
     const user = useSelector(getUser)
+    const { stack } = user
     return (
-    <section className="user__container">
-        <div className="user__background">
-            <div className="empty-avatar"></div>
-        </div>
-        <div className="user__data">
-            <div className="user__basic">{user.name} {user.mail} {user.phone}</div>
-            <div className="user__position">{user.position}</div>
-            <div className="user__description">{user.description}</div>
-        </div>
-        <div className="user__location">{user.description}</div>
-        <div className="user__social">
-            <a href={user.github} className="social-github"><img src={GithubWhite} alt=''/></a>
-            <a href={user.linkedin} className="social-github"><img src={LinkedinWhite} alt=''/></a>
-        </div>
-        <UserStack/>
-    </section>
-)}
+        <section>
+            <UserInfo user={user} />
+            <UserStack stack={stack} />
+        </section>
+    )
+}
 
 export default UserProfile;
