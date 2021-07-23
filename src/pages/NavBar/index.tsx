2c362e4fc64f7d5b547f503001486c45d93a982c
judithlient <judith@rzilient.club>
2021-07-23 16:34:31 +0200

    Fix icons import link

--- src/pages/NavBar/index.tsx
+++ src/pages/NavBar/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './index.scss'
-import { Burger, NuweLight, NuweLogo, Settings } from '../../assets'
+import { Burger, NuweLight, NuweLogo, Settings } from '../../assets/icons'
 import Button from './Button'
 
-const NavBar = (): JSX.Element =>  (
+const NavBar = (): JSX.Element => (
   <nav>
     <img src={Burger} alt='' />
     <div className="navbar__logo">
-      <img src={NuweLight} alt=''/>
-      <img src={NuweLogo} alt=''/>
+      <img src={NuweLight} alt='' />
+      <img src={NuweLogo} alt='' />
     </div>
     <Button className="navbar__settings">
-      <img src={Settings} alt=''/>
+      <img src={Settings} alt='' />
     </Button>
   </nav>
 );
