94a2666736b4566e57abc3ccff34cce1f4f5b420
judithmg <jmartigo33@gmail.com>
2021-07-24 15:11:22 +0200

    finish all profile tas

---
+++ src/components/ProfileTabs/Profile/ValidatedSkill.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./ValidatedSkill.scss";
+import * as logo from "../../../assets/icons/stack";
+
+type mapType = keyof typeof logo;
+type props = {
+  stack: {
+    type: mapType;
+    points: number;
+  }[];
+};
+
+const ValidatedSkills = ({ stack }: props): JSX.Element => (
+  <div className="validated-skills">
+    {stack &&
+      stack.map((el, index) => (
+        <div key={`${el}${index}`}>
+          <img key="el" src={logo[el.type] || ""} alt="" />
+          <span>Top 10%</span>
+          <span>{el.points}</span>
+        </div>
+      ))}
+  </div>
+);
+
+export default ValidatedSkills;
--- src/components/ProfileTabs/Profile/index.tsx
+++ src/components/ProfileTabs/Profile/index.tsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { texts } from '../../../constants'
+import React from "react";
+import { texts } from "../../../constants";
+import userType from "../../../types/userInterface";
+import RadarChart from "./RadarChart";
+import ValidatedSkill from "./ValidatedSkill";
+import "./index.scss";
+type props = {
+  user: userType;
+};
 
-const Profile = () : JSX.Element => (
-    <div>
-        <div className="profile__hard">
-            <h2>{texts.perfil.hard}</h2>
-            <div className="profile__top">
-                <h3>{texts.perfil.top}</h3>
-
-            </div>
-            <div className="profile__other">
-                <h3>{texts.perfil.other}</h3>
-
-            </div>
-        </div>
-
-        <div className="profile__soft">
-            <h2>{texts.perfil.soft}</h2>
-            <div className="profile__puntuaciones">
-                <h3>{texts.perfil.puntuaciones}</h3>
-            </div>
-        </div>
+const Profile = ({ user }: props): JSX.Element => (
+  <div className="profilenuwe__container">
+    <h2>{texts.perfil.hard}</h2>
+    <div className="profile__hard">
+      <div className="profile__top">
+        <h3>
+          {texts.perfil.top} {user.name}
+        </h3>
+        <RadarChart
+          data={user.hardSkills}
+          max={2000}
+          index="type"
+          color={"#56b951"}
+        />
+      </div>
+      <div className="profile__other">
+        <h3>{texts.perfil.other}</h3>
+        <ValidatedSkill stack={user.otherValidated} />
+      </div>
     </div>
-)
+
+    <h2>{texts.perfil.soft}</h2>
+    <div className="profile__soft">
+      <div className="profile__puntuaciones">
+        <h3>{texts.perfil.puntuaciones}</h3>
+      </div>
+      <RadarChart data={user.softSkills} max={5} index="type" color="orange" />
+    </div>
+  </div>
+);
 
 export default Profile;
