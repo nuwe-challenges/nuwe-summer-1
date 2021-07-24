7b7cb78b284bbabae94106bb2e6a5e3c644c2d46
judithmg <jmartigo33@gmail.com>
2021-07-24 23:54:43 +0200

    Add icons

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getMenu } from "../../redux/reducers/menu.reducer";
+import * as icon from "../../assets/icons";
 import "./index.scss";
 import { texts } from "../../constants";
 
@@ -9,11 +10,22 @@ const Menu = (): JSX.Element => {
   return (
     <div className={`menu ${menu ? "menu-open" : "menu-close"}`}>
       <ul>
-        <li>{texts.menu.perfil}</li>
-        <li>{texts.menu.panel}</li>
-        <li>{texts.menu.empresas}</li>
-        <li>{texts.menu.contacto}</li>
-        <li>{texts.menu.cerrar}</li>
+        <li>
+          <img src={icon.Home} alt="" />
+          {texts.menu.perfil}
+        </li>
+        <li>
+          <img src={icon.Work} alt="" />
+          {texts.menu.panel}
+        </li>
+        <li>
+          <img src={icon.Profile} alt="" />
+          {texts.menu.empresas}
+        </li>
+        <li>
+          <img src={icon.InfoCircle} alt="" />
+          {texts.menu.contacto}
+        </li>
       </ul>
     </div>
   );
--- src/pages/NavBar/index.tsx
+++ src/pages/NavBar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.scss";
-import { Burger, NuweLight, NuweLogo, Settings } from "../../assets/icons";
+import * as icon from "../../assets/icons";
 import { menuOpen } from "../../redux/reducers/menu.reducer";
 import Button from "./Button";
 import { useDispatch } from "react-redux";
@@ -11,14 +11,14 @@ const NavBar = (): JSX.Element => {
   return (
     <>
       <button type="button" onClick={handleClick} className="navbar__open">
-        <img src={Burger} alt="" className="navbar__burger" />
+        <img src={icon.Burger} alt="" className="navbar__burger" />
       </button>
       <div className="navbar__logo">
-        <img src={NuweLight} alt="" />
-        <img src={NuweLogo} alt="" />
+        <img src={icon.NuweLight} alt="" />
+        <img src={icon.NuweLogo} alt="" />
       </div>
       <Button className="navbar__settings">
-        <img src={Settings} alt="" />
+        <img src={icon.Settings} alt="" />
       </Button>
     </>
   );
