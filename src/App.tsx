beb55fa621c664308e78aa111273bd1eab4b317e
judithmg <jmartigo33@gmail.com>
2021-07-24 15:12:35 +0200

    Add all components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import './styles/index.scss'
-import NavBar from './pages/NavBar'
-import { ThemeProvider } from 'styled-components';
+import "./styles/index.scss";
+import NavBar from "./pages/NavBar";
+import Menu from "./pages/Menu";
 import UserProfile from "./pages/UserProfile";
 import Searching from "./components/Searching";
-import ProfileTabs from "./components/ProfileTabs";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./styles/index.scss');
+import ProfileTabs from "./pages/ProfileTabs";
 
 const App = (): JSX.Element => (
-  <ThemeProvider theme={theme}>
-    <NavBar />
-    <UserProfile />
-    <Searching />
-    <ProfileTabs />
-  </ThemeProvider>
+  <>
+    <nav>
+      <NavBar />
+    </nav>
+    <div id="app">
+      <aside>
+        <Menu />
+      </aside>
+      <main>
+        <UserProfile />
+        <Searching />
+        <ProfileTabs />
+      </main>
+    </div>
+  </>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import configureStore from "./redux/store/configureStore";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const store = configureStore()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./styles/index.scss');
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
