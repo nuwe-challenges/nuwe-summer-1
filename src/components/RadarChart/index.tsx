f31513bcd4c3bb719a876316df92ba81af9666c5
judithlient <judith@rzilient.club>
2021-07-27 16:32:11 +0200

    Change RadarChart route

---
+++ src/components/RadarChart/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ResponsiveRadar } from "@nivo/radar";
+import "./index.scss";
+
+const commonProperties = {
+  width: 200,
+  height: 200,
+  animate: true,
+};
+
+type props = {
+  data: {
+    type: string;
+    points: number;
+  }[];
+  max: number;
+  index: string;
+  color: string;
+};
+
+const RadarChart = ({ data, max, index, color }: props): JSX.Element => {
+  return (
+    <div className="radarchart">
+      <ResponsiveRadar
+        {...commonProperties}
+        data={data}
+        keys={["points"]}
+        indexBy={index}
+        maxValue={max}
+        curve="linearClosed"
+        borderWidth={4}
+        borderColor={color}
+        gridLevels={5}
+        gridShape="linear"
+        gridLabelOffset={30}
+        enableDots={false}
+        colors={color}
+        fillOpacity={0.25}
+        blendMode="normal"
+        animate={true}
+        isInteractive={true}
+      />
+    </div>
+  );
+};
+export default RadarChart;
