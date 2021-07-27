7cedfe43e1907cce27cacec9c7b7dee9fb484fa6
judithlient <judith@rzilient.club>
2021-07-27 16:32:38 +0200

    Add non functioning nft button

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GithubWhite, LinkedinWhite, Location } from "../../assets/icons";
 import userType from "../../types/userInterface";
 import "./index.scss";
@@ -8,12 +8,18 @@ type props = {
 };
 
 const UserInfo = ({ user }: props): JSX.Element => {
+
+  const [nft, setNft] = useState(false)
+
+  const handleOpen = () => setNft(prop => !prop)
+
   return (
     <div className="user__container">
       <div className="user__background">
         <div className="empty-avatar"></div>
       </div>
       <div className="user__data">
+        <button type='button' onClick={handleOpen}>VER NFT</button>
         <h2 className="user__basic">{user.name}</h2>
         <p>
           {user.mail} | {user.phone}
@@ -23,7 +29,7 @@ const UserInfo = ({ user }: props): JSX.Element => {
         <div className="user__location">
           <p>
             <img src={Location} alt="" />
-            {user.location}
+            {user.location.city}
           </p>
         </div>
         <div className="user__social">
